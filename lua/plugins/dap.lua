81bb7478ccead666afb1797df725ec2700ee7261
nick <fisher.nick14@gmail.com>
2024-05-15 20:38:35 -0700

    dap ui

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,12 +4,16 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = {
       "mfussenegger/nvim-dap-python",
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio", -- Add this line
     },
     config = function()
       local dap = require("dap")
       local dap_python = require("dap-python")
+      local dapui = require("dapui")
+      local nio = require("nio") -- Ensure nvim-nio is loaded
 
-      dap_python.setup("/usr/bin/python3", {}) -- Update this path
+      dap_python.setup("/usr/bin/python3") -- Update this path
 
       dap.adapters.python = {
         type = "executable",
@@ -25,6 +29,18 @@ return {
           program = "${file}",
         },
       }
+
+      dapui.setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
     end,
   },
 }
